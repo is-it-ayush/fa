2c5cacaf17aade1f29c6b91bed1e161d5f540e2a
Ayush Gupta <ayush.keshav2004@gmail.com>
2023-12-28 08:58:22 +0530

    chore: improve help

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,14 @@
-use clap::{Parser, Subcommand};
+use clap::{crate_authors, crate_version, Parser, Subcommand};
 
 #[derive(Parser, Debug, Clone)]
-#[command(author, version, about, long_about = None, arg_required_else_help = true)]
+#[command(
+    author,
+    version,
+    about,
+    arg_required_else_help = true,
+    disable_help_subcommand = true,
+    after_help = format!("Crafted By {} | {} | MIT", crate_authors!(), crate_version!())
+)]
 pub struct FaCli {
     #[command(subcommand)]
     pub command: Option<FaCommands>,
@@ -9,25 +16,21 @@ pub struct FaCli {
 
 #[derive(Subcommand, Debug, Clone)]
 pub enum FaCommands {
-    #[command(subcommand, about = "all other configuration related commands.")]
+    #[command(subcommand, about = "all configuration commands.")]
     Config(FaCommandConfig),
 
-    #[command(subcommand, about = "all other store related commands.")]
+    #[command(subcommand, about = "all store commands.")]
     Store(FaCommandStore),
 
     #[command(about = "list all the stored credentials.")]
     List {
-        #[arg(long, short, required = false, help = "an optional store name")]
+        #[arg(long, short, required = false, help = "an optional store name.")]
         store: Option<String>,
     },
 
     #[command(about = "add a new credential.")]
     Add {
-        #[arg(
-            index = 1,
-            help = "a required login.",
-            long_help = "this could be your email, username, login or any other identifier that goes along with your password."
-        )]
+        #[arg(index = 1, help = "a required username/email.")]
         user: String,
 
         #[arg(index = 2, help = "a required password.")]
@@ -39,22 +42,13 @@ pub enum FaCommands {
         #[arg(long, short, required = false, help = "an optional tag name.")]
         tag: Option<String>,
 
-        #[arg(
-            long,
-            short = 'w',
-            required = false,
-            help = "an optional associated website."
-        )]
+        #[arg(long, short = 'w', required = false, help = "an optional website.")]
         site: Option<String>,
     },
 
     #[command(about = "remove an existing credential.")]
     Remove {
-        #[arg(
-            index = 1,
-            help = "a required login.",
-            long_help = "this could be your email, username, login or any other identifier that goes along with your password."
-        )]
+        #[arg(index = 1, help = "a required username/email.")]
         user: String,
 
         #[arg(index = 2, help = "a required password.")]
@@ -66,51 +60,43 @@ pub enum FaCommands {
 
     #[command(about = "search through your store.")]
     Search {
-        #[arg(
-            index = 1,
-            help = "a required query.",
-            long_help = "internally, it uses the starts-with filter so if you search with 'jh', it'll list you all credentials whose 'logins' start with 'jh'."
-        )]
+        #[arg(index = 1, help = "a required search query.")]
         query: String,
 
         #[arg(long, short, required = false, help = "an optional store name.")]
         store: Option<String>,
 
-        #[arg(long, short, required = false)]
+        #[arg(
+            long,
+            short,
+            required = false,
+            help = "an optional filter.",
+            long_help = "an optional filter that is applied onto your search query. the format is <filter>/<filter_query> where <filter> can be either 'site' or 'tag' and <filter_query> would be your filter specific query."
+        )]
         filter: Option<String>,
     },
 
-    #[command(about = "initialize 'fa' & create configuration.")]
+    #[command(about = "initialize 'fa' and create a configuration.")]
     Init {
         #[arg(
             long,
             short,
-            help = "a required gpg key fingerprint.",
-            long_help = "this key will be used to encrypt/decrypt your stores. fingerprint is also known as key id sometimes. you can follow \"https://docs.github.com/en/authentication/managing-commit-signature-verification/telling-git-about-your-signing-key\" till step 4 to know to get your key fingerprint."
+            help = "a required gpg key-id/fingerprint.",
+            long_help = "a required gpg keyid/fingerprint. You can follow https://docs.github.com/en/authentication/managing-commit-signature-verification/telling-git-about-your-signing-key till 'Step 4' to get your gpg key-id/fingerprint."
         )]
         key_fingerprint: Option<String>,
 
-        #[arg(
-            long,
-            short = 'n',
-            help = "a required store name.",
-            long_help = "this will be your default store name. this is upto you to choose!"
-        )]
+        #[arg(long, short = 's', help = "a required store name.")]
         store: Option<String>,
 
-        #[arg(
-            long,
-            short = 'p',
-            help = "a required store path",
-            long_help = "this will be a directory where all the store's are located. i suggest creating a directory in a place you won't 'accidently' delete (lmao) and then doing 'git init'. then you can tell 'fa' about this directory via this flag and it'll save all the stores there."
-        )]
+        #[arg(long, short = 'p', help = "a required store path")]
         store_path: Option<String>,
     },
 }
 
 #[derive(Subcommand, Debug, Clone)]
 pub enum FaCommandConfig {
-    #[command(about = "view the current set of configuration used by 'fa'.")]
+    #[command(about = "view the current configuration utilized by 'fa'.")]
     View,
 }
 
@@ -125,13 +111,13 @@ pub enum FaCommandStore {
         store: String,
     },
 
-    #[command(about = "create an empty store.")]
-    Create {
+    #[command(about = "add a new empty store.")]
+    Add {
         #[arg(index = 1, help = "a required store name.")]
         store: String,
     },
 
-    #[command(about = "default a store.")]
+    #[command(about = "mark a store as default store.")]
     Default {
         #[arg(index = 1, help = "a required store name.")]
         store: String,
