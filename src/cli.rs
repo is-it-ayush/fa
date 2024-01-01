0752931802caddf249d06e89cfe0aa3220804c4d
Ayush Gupta <ayush.keshav2004@gmail.com>
2024-01-01 16:43:58 +0530

    chore: remove tracing for now

--- src/cli.rs
+++ src/cli.rs
@@ -71,7 +71,7 @@ pub enum FaCommands {
             short,
             required = false,
             help = "an optional filter.",
-            long_help = "an optional filter that is applied onto your search query. the format is <filter>/<filter_query> where <filter> can be either 'site' or 'tag' and <filter_query> would be your filter specific query."
+            long_help = "an optional filter that is applied onto your search query. The format is <filter>/<filter_query> where <filter> can be either 'site' or 'tag' and <filter_query> would be your filter specific query."
         )]
         filter: Option<String>,
     },
@@ -102,7 +102,7 @@ pub enum FaCommands {
             long,
             short = 'c',
             help = "a required csv file path.",
-            long_help = "a required csv file path. the only requirement is that the csv file must contain a 'username', 'password' field and an optional 'url' field."
+            long_help = "a required csv file path. The only requirement is that the csv file must contain a 'username', 'password' column and an optional 'url' column."
         )]
         csv_file: String,
     },
@@ -116,7 +116,7 @@ pub enum FaCommands {
             long,
             short = 'c',
             help = "a required csv file path.",
-            long_help = "a required csv file path. you can provide any name to the csv file. 'fa' will either overwrite or create one for you."
+            long_help = "a required csv file path. You can provide any name to the csv file. 'fa' will either overwrite or create one for you."
         )]
         csv_file: String,
     },
